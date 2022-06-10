92e8020e013095c06a551043542c8995fb75933f
EwigLife <zain@ewiglife.com>
2022-06-10 22:53:26 +0500

    build add lisitng now

--- lib/Controllers/listings_controller.dart
+++ lib/Controllers/listings_controller.dart
@@ -3,21 +3,20 @@ import 'package:get_storage/get_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:realestapp/Models/all_listing_model.dart';
 
+import '../Models/Categories/category_model.dart';
 import '../Models/locations_model.dart';
 
 class ListingController extends GetxController {
   var allListings = AllListings().obs;
   var myListings = AllListings().obs;
   var favoriteListings = AllListings().obs;
-  var locations = <LocationsModel>[].obs;
   final token = GetStorage().read('token');
-var categories;
+
   @override
   onInit() {
     getAllListing();
     getMyListing();
     getMyFavorites();
-    getLocation();
     super.onInit();
   }
 
@@ -69,20 +68,6 @@ var categories;
     );
 
     favoriteListings.value = allListingsFromJson(response.body);
-  }
-
-  getLocation() async {
-    String url = 'https://lagosabuja.com/wp-json/rtcl/v1/locations';
-    var response = await http.get(
-      Uri.parse(url),
-      headers: <String, String>{
-        'Accept': 'application/json',
-        'X-API-KEY': '835c5442-20ca-4d51-9e32-fae11c35fd42',
-        'Authorization': 'Bearer $token',
-      },
-    );
-
-    locations.value = locationsModelFromJson(response.body);
   }
 
   addListing(locationId, categoryId, listingType, title, status, price,
