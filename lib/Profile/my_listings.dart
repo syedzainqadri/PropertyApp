15d5f22fb4965483eda1f5cd4f34eeadb57fe389
EwigLife <zain@ewiglife.com>
2022-07-01 10:26:23 +0500

    delete controlled built

--- lib/Profile/my_listings.dart
+++ lib/Profile/my_listings.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:realestapp/Controllers/listings_controller.dart';
 import 'package:realestapp/Controllers/my_listings_controller.dart';
+import 'package:realestapp/Profile/Widgets/myLisitngCard.dart';
 
 import '../AddListings/list_widget.dart';
 import '../Utils/color_scheme.dart';
@@ -14,10 +15,10 @@ class MyListings extends StatefulWidget {
 }
 
 class _MyListingsState extends State<MyListings> {
-  final myListingController = Get.put(MyListingController());
   final ListingController listingController = Get.put(ListingController());
   @override
   Widget build(BuildContext context) {
+    var myListingController = Get.find<MyListingController>();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: transparent,
@@ -40,30 +41,30 @@ class _MyListingsState extends State<MyListings> {
       body: Obx(() {
         return listingController.isLoading == true
             ? const CircularProgressIndicator()
-            : GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2),
-                padding: const EdgeInsets.only(left: 18.0),
+            : ListView.builder(
+                padding: const EdgeInsets.only(left: 18.0, right: 18),
                 scrollDirection: Axis.vertical,
                 shrinkWrap: true,
                 itemCount: myListingController.myListings.value.data?.length,
                 itemBuilder: (context, index) {
-                  return ListingCard(
-                      image: myListingController
-                          .myListings.value.data![index].images,
-                      title: myListingController
-                          .myListings.value.data![index].title
-                          .toString(),
-                      city: myListingController.myListings.value.data![index]
-                          .contact!.locations![0].name
-                          .toString(),
-                      price: myListingController
-                          .myListings.value.data![index].price
-                          .toString(),
-                      isFovorite: false,
-                      description: '',
-                      listingId: myListingController
-                          .myListings.value.data![index].listingId);
+                  return Obx(
+                    () => MyListingCard(
+                        image: myListingController
+                            .myListings.value.data![index].images,
+                        title: myListingController
+                            .myListings.value.data![index].title
+                            .toString(),
+                        city: myListingController.myListings.value.data![index]
+                            .contact!.locations![0].name
+                            .toString(),
+                        price: myListingController
+                            .myListings.value.data![index].price
+                            .toString(),
+                        isFovorite: false,
+                        description: '',
+                        listingId: myListingController
+                            .myListings.value.data![index].listingId),
+                  );
                 },
               );
       }),
